8082451d238bb5331c53a66cc4511e2d23eddcbc
Matthew Abbott <matt@ingeniumsoftware.dev>
2021-09-23 13:00:16 +0100

    Build extension example

---
+++ build-extensions/FrameworkBuildExtensions/DoSomethingTask.cs
@@ -0,0 +1,34 @@
+﻿namespace FrameworkBuildExtensions
+{
+	using Build;
+	using Build.Tasks;
+
+	using Cake.Core.Diagnostics;
+	using Cake.Frosting;
+
+	[TaskName("DoSomething")]
+	[IsDependeeOf(typeof(ResolveVerson))]
+	public class DoSomethingTask : BuildTask
+	{
+		protected override void RunCore(BuildContext context)
+		{
+			context.Log.Write(
+				Verbosity.Normal,
+				LogLevel.Error,
+				"Hello from build extension");
+		}
+	}
+
+	public class AfterBuildHook : BuildHook
+	{
+		public override void BeforeBuild(BuildContext context, BuildProject project)
+		{
+			context.Log.Information($"Before building project: {project.Name}");
+		}
+
+		public override void AfterBuild(BuildContext context, BuildProject project)
+		{
+			context.Log.Information($"After building project: {project.Name}");
+		}
+	}
+}
